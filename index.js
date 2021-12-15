5cf1b2f982816c50d12228f3c8e4a4d53bd68218
Josh Fuller <joshf5252@gmail.com>
2021-12-14 23:25:00 -0500

    commit tree 7cd56394bc9cb0a3d9a85ae0e504d0dc2a2ddc5c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase())!== -1
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    let correctLetters = drivers.filter(driver => driver.slice(0,string.length) == string)
+    return correctLetters
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
